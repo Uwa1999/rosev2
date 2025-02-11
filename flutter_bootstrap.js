6f11e251f5d1afc4b87230da2d5596165d6621f1
FDSAP-Joshua <111028254+Uwa1999@users.noreply.github.com>
2025-02-11 10:12:30 +0800

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "836657340"
+    serviceWorkerVersion: "2442232799"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b7ccb1dc1746ab85731aaa0efaa9e89b",
+const RESOURCES = {"flutter_bootstrap.js": "3add1da32d6337b80068f606230a1f8a",
 "version.json": "ddff596fd57d49e508934e01d0c8ae65",
 "index.html": "2bf625e98631db4941e21bc23196e09b",
 "/": "2bf625e98631db4941e21bc23196e09b",
-"main.dart.js": "fc30365ced7eb0af02bf369753eb507f",
+"main.dart.js": "318b1063ca04095a00fc6304233ca5b8",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "otto.png": "1559be76e554b564380711f56bbe3850",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
